cad230820f23abe1ddc46e5bf2448774d91f696b
XiaoBai-2017 <15817223385@163.com>
2018-12-04 11:53:15 +0800

    add OWSAStartupSingleton Static class to init and startup winsocket

--- Src/OWin32Socket.h
+++ Src/OWin32Socket.h
@@ -6,3 +6,5 @@ Content     :   Windows-specific socket-based networking implementation
 Created     :   December 4, 2018
 Authors     :   defeng.liang
 ************************************************************************************/
+
+#include "OSocket.h"
